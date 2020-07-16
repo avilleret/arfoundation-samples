c41e58a72900fe5e798325b870fc49565636b7b8
Antoine Villeret <antoine@makula.be>
2020-07-16 16:51:53 +0200

    add dropdown menu in SimpleOcclusion scene to change Environment Depth Mode on the fly

---
+++ Assets/EnvironmentDepthModeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class EnvironmentDepthModeController : MonoBehaviour
+{
+    AROcclusionManager occlusionManager;
+
+    void Awake()
+    {
+        occlusionManager = FindObjectOfType<AROcclusionManager>();
+    }
+
+    public void OnDropdownValueChanged(int val)
+    {
+        switch(val)
+        {
+            default:
+            case 0:
+              occlusionManager.requestedEnvironmentDepthMode = EnvironmentDepthMode.Disabled;
+              break;
+            case 1:
+              occlusionManager.requestedEnvironmentDepthMode = EnvironmentDepthMode.Fastest;
+              break;
+            case 2:
+              occlusionManager.requestedEnvironmentDepthMode = EnvironmentDepthMode.Medium;
+              break;
+            case 3:
+              occlusionManager.requestedEnvironmentDepthMode = EnvironmentDepthMode.Best;
+              break;
+        }
+
+    }
+}
